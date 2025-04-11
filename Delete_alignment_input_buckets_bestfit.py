e5a2e9b600904366d02e8482232a55b23f3389f6
sarahavini <146227158+sarahavini@users.noreply.github.com>
2025-04-11 10:54:01 -0500

    Add files via upload
    Code snippet for new way to delete inputs from an alignment in inspire 2025.1.169.0

---
+++ Delete_alignment_input_buckets_bestfit.py
@@ -0,0 +1,28 @@
+
+bestfitobject = IN_Alignment("Alignments/Best Fit to Points Alignment1")
+
+#Get actual children
+inputactuals = bestfitobject.ActualInputBucket.Path
+t = IN_GetChildren(List[str]([inputactuals])).Run()
+print(str(t.ChildrenNamesList))
+
+#make the full path to the children to delete them
+for i in t.ChildrenNamesList:
+    fullpath = inputactuals + "/" + i
+    print(fullpath)
+    cmd6 = IN_Delete(List[str]([str(fullpath)])).Run()
+
+
+
+
+#Get nominal children
+inputnominals = bestfitobject.NominalInputBucket.Path
+t = IN_GetChildren(List[str]([inputnominals])).Run()
+print(str(t.ChildrenNamesList))
+
+#make the full path to the children to delete them
+for i in t.ChildrenNamesList:
+    fullpath = inputnominals + "/" + i
+    print(fullpath)
+    cmd6 = IN_Delete(List[str]([str(fullpath)])).Run()
+
